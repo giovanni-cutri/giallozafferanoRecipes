15e8d05a02fc0782b2b5e826a920c818a61d997a
Giovanni Cutrì <89142084+giovanni-cutri@users.noreply.github.com>
2023-05-22 13:17:03 +0200

    Add get_recipes_links function

---
+++ R/get_recipes_links.R
@@ -0,0 +1,21 @@
+library(rvest)
+
+get_recipes_links <- function(url) {
+
+  recipes_links <- c()
+
+  while(!identical(url, character(0))){
+    recipes_page <- read_html(url)
+    recipes <- recipes_page %>%
+      html_elements(css = ".gz-title a") %>%
+      html_attr("href")
+    recipes_links <- append(recipes_links, recipes)
+    next_page <- recipes_page %>%
+      html_elements(css = ".next") %>%
+      html_attr("href")
+    url <- next_page
+  }
+
+  return(recipes_links)
+
+}
