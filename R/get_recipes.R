48b9dee62288211af75678b490d81f4ce10a3d5c
Giovanni Cutrì <89142084+giovanni-cutri@users.noreply.github.com>
2023-05-22 18:10:22 +0200

    Add support for search by recipe category

--- R/get_recipes.R
+++ R/get_recipes.R
@@ -2,12 +2,150 @@ source("update_url.R")
 source("get_recipes_links.R")
 source("get_recipe_info.R")
 
-get_recipes <- function(difficulty = "", cooking_time = "", diet = "",
+get_recipes <- function(category = "", difficulty = "", cooking_time = "", diet = "",
                         ingredient = "", cooking_method = ""){
   
   print("Generating query...")
   
-  url <- "https://www.giallozafferano.it/ricette-cat/?search_query="
+  url <- "https://www.giallozafferano.it/ricette-cat/"
+  
+  if (category == "antipasti"){
+    url <- update(url, "Antipasti/")
+  }
+  
+  else if (category == "primi"){
+    url <- update(url, "Primi/")
+  }
+  
+  else if (category == "pasta"){
+    url <- update(url, "Primi/pasta/")
+  }
+  
+  else if (category == "pasta_fresca"){
+    url <- update(url, "Primi/Pasta-fresca/")
+  }
+  
+  else if (category == "gnocchi"){
+    url <- update(url, "Primi/Gnocchi/")
+  }
+  
+  else if (category == "riso_cereali"){
+    url <- update(url, "Primi/riso-cereali/")
+  }
+  
+  else if (category == "zuppe_minestre_vellutate"){
+    url <- update(url, "Primi/zuppe-minestre-vellutate/")
+  }
+  
+  else if (category == "secondi"){
+    url <- update(url, "Secondi-piatti/")
+  }
+  
+  else if (category == "piatti_unici"){
+    url <- update(url, "Piatti-Unici/")
+  }
+  
+  else if (category == "dolci"){
+    url <- update(url, "Dolci-e-Desserts/")
+  }
+  
+  else if (category == "torte"){
+    url <- update(url, "Dolci-e-Desserts/Torte/")
+  }
+  
+  else if (category == "al_cucchiaio"){
+    url <- update(url, "Dolci-e-Desserts/Al-cucchiaio/")
+  }
+  
+  else if (category == "biscotti"){
+    url <- update(url, "Dolci-e-Desserts/Biscotti/")
+  }
+  
+  else if (category == "gelati_sorbetti_semifreddi"){
+    url <- update(url, "Dolci-e-Desserts/Gelati-e-Semifreddi/")
+  }
+  
+  else if (category == "salse_creme"){
+    url <- update(url, "Dolci-e-Desserts/Salse-e-Creme/")
+  }
+  
+  else if (category == "piccola_pasticceria"){
+    url <- update(url, "Dolci-e-Desserts/piccola-pasticceria/")
+  }
+  
+  else if (category == "cheesecake"){
+    url <- update(url, "Dolci-e-Desserts/Cheesecakes/")
+  }
+  
+  else if (category == "contorni"){
+    url <- update(url, "Contorni/")
+  }
+  
+  else if (category == "insalate"){
+    url <- update(url, "Insalate/")
+  }
+  
+  else if (category == "torte_salate"){
+    url <- update(url, "Torte-salate/")
+  }
+  
+  else if (category == "lievitati"){
+    url <- update(url, "Lievitati/")
+  }
+  
+  else if (category == "pizze_focacce"){
+    url <- update(url, "Lievitati/Pizze-e-focacce/")
+  }
+  
+  else if (category == "pane"){
+    url <- update(url, "Lievitati/pane/")
+  }
+  
+  else if (category == "lievitati_dolci"){
+    url <- update(url, "Lievitati/Dolci/")
+  }
+  
+  else if (category == "salse_sughi"){
+    url <- update(url, "Salse-e-Sughi/")
+  }
+  
+  else if (category == "sughi"){
+    url <- update(url, "Salse-e-Sughi/Sughi/")
+  }
+  
+  else if (category == "salse_condimenti"){
+    url <- update(url, "Salse-e-Sughi/Salse-e-condimenti/")
+  }
+  
+  else if (category == "marmellate_conserve"){
+    url <- update(url, "Marmellate-e-Conserve/")
+  }
+  
+  else if (category == "marmellate"){
+    url <- update(url, "Marmellate-e-Conserve/Marmellate/")
+  }
+  
+  else if (category == "conserve"){
+    url <- update(url, "Marmellate-e-Conserve/Conserve/")
+  }
+  
+  else if (category == "bevande"){
+    url <- update(url, "Bevande/")
+  }
+  
+  else if (category == "analcolici"){
+    url <- update(url, "Bevande/Analcolici/")
+  }
+  
+  else if (category == "alcolici"){
+    url <- update(url, "Bevande/Alcolici/")
+  }
+  
+  else if (category == "frappe_frullati"){
+    url <- update(url, "Bevande/frappe-frullati/")
+  }
+  
+  url <- update(url, "?search_query=")
   
   if (difficulty == "facile"){
     url <- update(url, "&difficolta%5Bfacile%5D=1")
